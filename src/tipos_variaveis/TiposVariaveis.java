fa41d06ca45121ea0ff5552ef06951c084868547
Davy-Santos <davy.santos06@gmail.com>
2025-04-03 19:44:32 -0300

    Operadores Lógicos

--- src/tipos_variaveis/TiposVariaveis.java
+++ src/tipos_variaveis/TiposVariaveis.java
@@ -2,7 +2,17 @@ package tipos_variaveis;
 
 public class TiposVariaveis {
 	public static void main (String [] args) {
-		System.out.print("Hello World!");
+		boolean condicao1 = true;
+		boolean condicao2 = true;
+		
+		if(condicao1 && (7 > 4)) {
+			System.out.println("Uma das condiçôes é verdadeira");
+		}
+		
+		if(condicao1 ||condicao2) {
+			System.out.println("as duas condiçôes são verdadeiras");
+			}
+		System.out.println("fim");
 	}
 
 }
